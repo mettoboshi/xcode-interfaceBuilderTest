33d1a545a3ff06da44d63f9865fadbb98f606d6a
mettoboshi <mettoboshi.1108@gmail.com>
2014-07-13 11:58:03 +0900

    Date Test

--- interfaceBuilder1/ViewController.m
+++ interfaceBuilder1/ViewController.m
@@ -19,7 +19,8 @@
   [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-  
+  NSDate *theDate = [[NSDate alloc] init];
+  NSLog(@"date : %@", theDate);
   
 }
 
